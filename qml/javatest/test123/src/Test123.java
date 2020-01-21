8928aa7ca665524e5ae7366efcb674ebf52f510c
ThomasR <thomasrutgers@gmail.com>
2020-01-21 17:04:04 +0100

    Add files via upload

---
+++ qml/javatest/test123/src/Test123.java
@@ -0,0 +1,16 @@
+
+import java.util.concurrent.*;
+public class Test123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		TimeUnit.SECONDS.sleep(3);
+		}
+		catch(InterruptedException i) {
+		
+		}
+		System.out.println(Math.random());
+	}
+
+}
